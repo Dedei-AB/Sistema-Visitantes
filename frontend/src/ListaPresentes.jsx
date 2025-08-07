269bcf391e7ca2bba1de78314d37f4d9a6ae2f48
Maria Eduarda <163424987+mariamacedos2@users.noreply.github.com>
2025-08-07 15:55:48 -0300

    07-08

--- frontend/src/ListaPresentes.jsx
+++ frontend/src/ListaPresentes.jsx
@@ -25,7 +25,19 @@ export default function PessoasPresentes({ pessoas, onSaida }) {
               <button onClick={() => onSaida(pessoa.id)} className="botao-saida">Registrar Saída</button>
             </div>
             <div className="dados-pessoa">
-              <p><strong>Nome:</strong> {pessoa.nome}</p>
+
+              <p className="nome"><strong>
+              <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-person-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+            </svg>  </strong> {pessoa.nome}</p>
+
               <div className="linha-horizontal">
               <p><strong>CPF:</strong> {pessoa.cpf}</p>
               <p><strong>Telefone:</strong> {pessoa.telefone}</p>
