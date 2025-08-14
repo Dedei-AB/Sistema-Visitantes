52859f05417a91b5e2018a7699b7837415a8d411
Dedei-AB <andreiabners@gmail.com>
2025-08-14 16:13:21 -0300

    Calendário

--- frontend/src/ListaCadastrados.jsx
+++ frontend/src/ListaCadastrados.jsx
@@ -1,9 +1,22 @@
+import { DateRange } from "react-date-range";
+import { addDays } from "date-fns";
 import { useEffect, useState } from "react";
 import Pessoas from "./Pessoas";
 import "./ListaCadastrados.css";
+import "react-date-range/dist/styles.css"; // estilo principal
+import "react-date-range/dist/theme/default.css"; // tema default
 
 function ListaCadastrados() {
+  const [periodo, setPeriodo] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 7),
+      key: "selection",
+    },
+  ]);
   const [dado, setDado] = useState([]);
+  const [showCalendar, setShowCalendar] = useState(false);
+
   useEffect(() => {
     fetch("http://localhost:5000/visitas/pessoa_visita")
       .then((response) => response.json())
@@ -22,9 +35,42 @@ function ListaCadastrados() {
 
       <h2>Visitas:</h2>
 
+      <div className="data-filtro-cadastrados">
+        <h3>Selecione um período:</h3>
+        <span
+          className="descricao-calendario"
+          onClick={() => setShowCalendar(!showCalendar)}
+        >
+          <i class="bi bi-calendar-range"></i>
+          {periodo[0].startDate.toLocaleDateString()} -{" "}
+          {periodo[0].endDate.toLocaleDateString()}
+        </span>
+
+        {showCalendar && (
+          <span className="calendario-cadastrados">
+            <DateRange
+              editableDateInputs={true}
+              onChange={(item) => setPeriodo([item.selection])}
+              ranges={periodo}
+              dateDisplayFormat="dd-MM-yyyy"
+              direction="horizantal"
+              months={1}
+            />
+            <button
+              onClick={() => {
+                setPeriodo(periodo); // confirma o período
+                setShowCalendar(false); // fecha o calendário
+              }}
+              className="botao-calendario"
+            >
+              X
+            </button>
+          </span>
+        )}
+      </div>
+
       <div className="tabela-lista-cadastrados">
         {dado.map((pessoa, index) => {
-          console.log(pessoa);
           return <Pessoas key={index} visitas={pessoa} />;
         })}
       </div>
